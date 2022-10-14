b4b3f61b5ab0eb48a7ef8cf36e78427ec80492c2
Max07092001 <mayukhacharya2001@gmail.com>
2022-10-14 23:25:14 +0530

    added src

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import ShopCreate_Ac from "./ShopCreate_Ac";
-import ShopCreate_Ac2 from "./ShopCreate_Ac2";
-import Signin from "./Signin";
+import ShopCreate_Ac from "./src/Register/User/ShopCreate_Ac";
+import ShopCreate_Ac2 from "./src/Register/User/ShopCreate_Ac2";
+import Signin from "./src/Login/Signin";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="LoginPage">
-      <Stack.Screen name="signin" component={Signin} />
+        <Stack.Screen name="signin" component={Signin} />
         <Stack.Screen name="1" component={ShopCreate_Ac} />
         <Stack.Screen name="2" component={ShopCreate_Ac2} />
       </Stack.Navigator>

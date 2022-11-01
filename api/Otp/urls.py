3fc2f076e91c4a80c9eb4167af54e07120340bae
Sagnik Banik <85645458+sagnik036@users.noreply.github.com>
2022-11-01 22:10:56 +0530

    Delete api directory

--- api/Otp/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path,include
-
-urlpatterns = [
-    path(
-        'otpGenerate/<str:username>',
-        views.getUsernameNumberRegistered.as_view()
-    ),
-]
--- api/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-from api.users.models import *
-from api.Otp.models import *
-# Create your models here.
